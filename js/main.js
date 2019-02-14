eadc26ddecd36d7c19bf980a11cc63fb852d5000
John Jacobs <john.a.jacobs1973@gmail.com>
2019-02-14 16:20:27 -0500

    guessed letter stored in var

--- js/main.js
+++ js/main.js
@@ -2,9 +2,19 @@ var playWord = ['dog', 'cat']
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
         'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
         't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var guessLet;
 var buttons = function(){
-    $('.buttons').append('<ul id="buttons"></ul>');
+    $('.buttons').append('<ul id="buttons"></ul>')
     for (var i = 0; i < alphabet.length; i++) {
-    $('#buttons').append(`<li>${alphabet[i]}</li>`);
+    $('#buttons').append(`<li>${alphabet[i]}</li>`)
+    $('#buttons').on('click', 'li', function(event){
+        event.preventDefault()
+        guessLet = $(this).text()
+    })
     }
 }
+
+$('#start').on('click', function() {
+    buttons()
+})
+
