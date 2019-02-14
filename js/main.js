ae09dda0da7922148b5e2cfd2ecffc3b60b4aa77
John Jacobs <john.a.jacobs1973@gmail.com>
2019-02-14 15:55:00 -0500

    js to create letter elements with letters in them

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
+var playWord = ['dog', 'cat']
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
         'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
         't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var buttons = function(){
     $('.buttons').append('<ul id="buttons"></ul>');
     for (var i = 0; i < alphabet.length; i++) {
-    $('#buttons').append(`<li></li>`);
-    $('#buttons li').text(alphabet[i])
-    console.log($('#buttons li'))
+    $('#buttons').append(`<li>${alphabet[i]}</li>`);
     }
 }
