8145f36a5681b63883aa1f4e95a38407902b080e
John Jacobs <john.a.jacobs1973@gmail.com>
2019-02-15 13:33:00 -0500

    lose function added

--- js/main.js
+++ js/main.js
@@ -1,54 +1,63 @@
-var wordList = ['dog', 'cat','badger', 'dude','lorem', 'bottle'];
+var wordList = ['dog', 'cat', 'badger', 'dude', 'lorem', 'bottle'];
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-        't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var wordInPlay;
 var score = 0;
 var guessLet;
-var guessList = [];
+var guessGood = 0;
+var badCounter = 0;
 var guessBad = 0;
-var buttons = function(){
-    $('.buttons').append('<ul id="buttons"></ul>')
+var buttons = function () {
+    $('.buttons').append('<ul id="buttons"></ul>')  //Create buttons for alphabet
     for (var i = 0; i < alphabet.length; i++) {
-    $('#buttons').append(`<li>${alphabet[i]}</li>`)
-    } 
-    $('#buttons').on('click', 'li', function(event){
+        $('#buttons').append(`<li>${alphabet[i]}</li>`)
+    }
+    $('#buttons').on('click', 'li', function (event) {
         event.preventDefault()
         guessLet = $(this).text()
-        
-        $('.hidden div').each( function() {         //Checking for a match
-            if ($(this).text() === guessLet) {
+        $('.hidden div').each(function () {
+            if ($(this).text() === guessLet) {      //Checking for a match
                 $(this).css('color', 'red')         //changes color to unhide
-                console.log("match!")
-                guessList.push(guessLet)
-                // console.log(guessList)
-                if (guessList.length === wordInPlay.length) {
-                    $('h1').text("YOU WIN!")
-                    score +=100
-                    $('.score').append(`<div>${score}</div>`)
+                guessGood++
+                if (guessGood === wordInPlay.length) {
+                    winFunc()
                 }
             } else {
                 guessBad++
-                console.log(guessBad)
+                if (guessBad === wordInPlay.length) {
+                    badCounter++
+                    guessBad = 0
+                    if (badCounter > 5) {
+                        loseFunc()
+                    }
+                }
+                
             }
-            
-            
         })
         $(this).remove()
-        
     })
-};
-var  chooseWord = function(){
-    let randomWordInd = Math.floor(Math.random()*wordList.length)
+}
+var chooseWord = function () {
+    let randomWordInd = Math.floor(Math.random() * wordList.length)
     wordInPlay = wordList[randomWordInd]
     wordList.splice(randomWordInd, 1)
-     let arr = wordInPlay.split("")
+    let arr = wordInPlay.split("")
     for (var i = 0; i < arr.length; i++) {
         $('.hidden').append(`<div>${arr[i]}</div>`)
-        }      
-     
+    }
+};
+var winFunc = function () {
+    $('h1').text("YOU WIN!")
+    score += 100
+    $('.score').append(`<div>${score}</div>`)
+};
+var loseFunc = function () {
+    $('h1').text("LOSER!")
+    score -= 100
+    $('.score').append(`<div>${score}</div>`)
 }
-$('#start').on('click', function() {
+$('#start').on('click', function () {
     buttons()
     chooseWord()
-});
+})
